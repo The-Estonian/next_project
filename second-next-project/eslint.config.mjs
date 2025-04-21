5fc4e04c91ee97de59c33acfb38515c40b534045
jaanus <zaar2213@gmail.com>
2025-04-21 21:42:42 +0300

    started new next.js project

---
+++ second-next-project/eslint.config.mjs
@@ -0,0 +1,23 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        rules: {
+            // 2-space indentation
+            indent: ['error', 4, { SwitchCase: 1 }],
+        },
+    },
+];
+
+export default eslintConfig;
